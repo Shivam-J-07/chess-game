bdde86d36229b42c1ee8e73285b3a5abd7a77951
Shivam-J-07 <Shivamganeshjindal07@gmail.com>
2023-03-28 23:50:16 -0700

    Created chess board

--- src/App.js
+++ src/App.js
@@ -1,25 +1,55 @@
-import logo from './logo.svg';
-import './App.css';
+import { Chessboard } from "react-chessboard";
+import { Chess } from "chess.js";
+import { useState } from "react";
 
 function App() {
+  const [game, setGame] = useState(new Chess());
+
+  const pieceDrop = (source, target) => {
+    console.log(source, target)
+    const newGame = new Chess(game.fen())
+    let move;
+    try {
+      move = newGame.move({
+        from: source,
+        to: target
+      })
+    } catch (e) {
+      console.log(e)
+    }
+    setGame(newGame)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="w-screen h-screen flex justify-center bg-[#4D2E19]">
+        <div className="my-auto">
+          <Chessboard position={game.fen()} boardWidth={750} onPieceDrop={(source, target) => pieceDrop(source, target)}/>
+        </div>
+      </div>
+    </>
   );
 }
 
 export default App;
+
+  // const makeRandomMove = () => {
+  //   const possibleMove = game.move();
+
+  //   if (game.isGameOver() || game.isDraw() || possibleMove.length() === 0)
+  //     return;
+
+  //   const randomIndex = Math.floor(Math.random() * possibleMove.length);
+  //   //play random move
+  //   safeGameMutate((game) => {
+  //     game.move(possibleMove[randomIndex]);
+  //   });
+  // };
+
+  // const safeGameMutate = (modify) => {
+  //   setGame((g) => {
+  //     const update = { ...g };
+  //     modify(update);
+  //     return update;
+  //   });
+  // };
